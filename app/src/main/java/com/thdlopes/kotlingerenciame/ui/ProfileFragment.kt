a0be57d1321adee272335daf7928015645481c97
Thiago Lopes <thiagodaniellopes99@gmail.com>
2021-06-07 12:45:11 -0300

    Adiciona nome e edição para nome

--- app/src/main/java/com/thdlopes/kotlingerenciame/ui/ProfileFragment.kt
+++ app/src/main/java/com/thdlopes/kotlingerenciame/ui/ProfileFragment.kt
@@ -1,20 +1,19 @@
 package com.thdlopes.kotlingerenciame.ui
 
+import android.R
 import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.util.Patterns
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
-import com.thdlopes.kotlingerenciame.data.FinanceViewModel
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.thdlopes.kotlingerenciame.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
 
@@ -24,6 +23,7 @@ class ProfileFragment : Fragment() {
     private lateinit var firebaseAuth: FirebaseAuth
 
     private var newEmail = ""
+    private var newName = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +34,10 @@ class ProfileFragment : Fragment() {
         if (firebaseUser != null){
             val email = firebaseUser.email
             val uid = firebaseUser.uid
+            val name = firebaseUser.displayName
             binding.textViewUserEmail.text = email
             binding.textViewShowUid.text = uid
+            binding.textViewUserName.text = name
         }else{
             startActivity(Intent(requireContext(), LoginActivity::class.java))
         }
@@ -58,14 +60,21 @@ class ProfileFragment : Fragment() {
         var email = firebaseUser!!.email
         checkUser()
 
+        binding.textViewEditName.setOnClickListener {
+            validateNewName()
+        }
+
         binding.textViewEditEmail.setOnClickListener {
             validateNewEmail()
-
         }
 
         binding.textViewResetPassword.setOnClickListener {
             firebaseAuth.sendPasswordResetEmail(email!!)
-            Toast.makeText(requireContext(), "Verifique seu e-mail para alterar a senha", Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                requireContext(),
+                "Verifique seu e-mail para alterar a senha",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -83,15 +92,53 @@ class ProfileFragment : Fragment() {
         val firebaseUser = firebaseAuth.currentUser
         AlertDialog.Builder(requireContext()).also {
             it.setTitle("Confirmar alteração de e-mail")
-            it.setMessage("Você precisará relogar depois de alterar seu e-mail e verificá-lo.Tem certeza que quer alterar seu e-mail para $newEmail?" )
+            it.setMessage("Você precisará relogar depois de alterar seu e-mail e verificá-lo.Tem certeza que quer alterar seu e-mail para $newEmail?")
             it.setPositiveButton("Sim, alterar"){ dialog, which ->
                 firebaseUser!!.updateEmail(newEmail).addOnSuccessListener {
                     firebaseUser.sendEmailVerification()
                     firebaseAuth.signOut()
                     startActivity(Intent(requireContext(), LoginActivity::class.java))
                 }
-                Toast.makeText(context, "E-mail alterado. Verifique o novo e-mail para prosseguir.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    context,
+                    "E-mail alterado. Verifique o novo e-mail para prosseguir.",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }.create().show()
     }
+
+    private fun validateNewName() {
+        newName = binding.editTextNewName.text.toString().trim()
+        if (newName.isEmpty()){
+            binding.editTextNewName.error = "Campo em branco"
+        } else {
+            updateName()
+        }
+    }
+
+    private fun updateName(){
+        firebaseAuth = FirebaseAuth.getInstance()
+        val firebaseUser = firebaseAuth.currentUser
+        var updateProfileName = UserProfileChangeRequest.Builder()
+            .setDisplayName(newName)
+            .build()
+        firebaseUser?.updateProfile(updateProfileName)
+            ?.addOnSuccessListener {
+                Toast.makeText(requireContext(), "Nome alterado.", Toast.LENGTH_SHORT).show()
+                reloadFragment()
+            }
+            ?.addOnFailureListener { e ->
+                Toast.makeText(
+                    requireContext(),
+                    "Não foi possível concluir devido a ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun reloadFragment(){
+        checkUser()
+        binding.editTextNewName.text?.clear()
+       }
 }
